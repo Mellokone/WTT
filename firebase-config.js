f820203df7e0bb652b82a5132b1f4ab5135bde95
Mellok <mellokone@gmail.com>
2026-03-27 02:26:10 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCqJF0igmV7urQA1N7uzgsasywPTGNItmE",
-  authDomain: "weekly-trade-tracker.firebaseapp.com",
-  projectId: "weekly-trade-tracker",
-  storageBucket: "weekly-trade-tracker.firebasestorage.app",
-  messagingSenderId: "825836238795",
-  appId: "1:825836238795:web:10724501068f6b56fae84e",
-  measurementId: "G-E5RJY0C16C"
-};
