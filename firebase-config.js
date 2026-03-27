4e9104805d6f276826e7aa802f1b73ddf225a10e
Mellok <mellokone@gmail.com>
2026-03-27 02:27:17 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.WTT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCqJF0igmV7urQA1N7uzgsasywPTGNItmE",
+  authDomain: "weekly-trade-tracker.firebaseapp.com",
+  projectId: "weekly-trade-tracker",
+  storageBucket: "weekly-trade-tracker.firebasestorage.app",
+  messagingSenderId: "825836238795",
+  appId: "1:825836238795:web:10724501068f6b56fae84e",
+  measurementId: "G-E5RJY0C16C"
+};
